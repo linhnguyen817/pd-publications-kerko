e52c3aa2b735c7deb35742b00319243a44329bb3
Linh Nguyen <linhn@mit.edu>
2020-05-21 19:24:41 -0500

    add datadir

--- kerkoapp.py
+++ kerkoapp.py
@@ -16,6 +16,7 @@ app.config['SECRET_KEY'] = 'K#d{c^#KgD=s.Kq'  # Replace this value.
 app.config['KERKO_ZOTERO_API_KEY'] = 'DUTwl21GgxVc74bGODObxDut'  # Replace this value.
 app.config['KERKO_ZOTERO_LIBRARY_ID'] = '5778088'  # Replace this value.
 app.config['KERKO_ZOTERO_LIBRARY_TYPE'] = 'user'  # Replace this value if necessary.
+app.config['KERKO_DATA_DIR'] = str('data' / 'kerko')
 app.config['KERKO_COMPOSER'] = Composer()
 
 babel = Babel(app)
