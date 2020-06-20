440e4e2df7b8e9d7d2206febc9453d2df9233596
Linh Nguyen <linhn@mit.edu>
2020-06-19 20:58:44 -0500

    Update 6-19 for newsletter

--- kerkoapp.py
+++ kerkoapp.py
@@ -8,6 +8,7 @@ from kerko import blueprint as kerko_blueprint
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return redirect(url_for('kerko.search'))
@@ -17,11 +18,12 @@ app.config['SECRET_KEY'] = 'K#d{c^#KgD=s.Kq'  # Replace this value.
 app.config['KERKO_ZOTERO_API_KEY'] = 'DUTwl21GgxVc74bGODObxDut'  # Replace this value.
 app.config['KERKO_ZOTERO_LIBRARY_ID'] = '5778088'  # Replace this value.
 app.config['KERKO_ZOTERO_LIBRARY_TYPE'] = 'user'  # Replace this value if necessary.
+app.config['KERKOAPP_COLLECTION_FACETS']= 'LKGKEMQ6:110:Positive Deviance Publications;'
+app.config['KERKOAPP_EXCLUDE_DEFAULT_FACETS'] = 'facet_tag'
 app.config['KERKO_DATA_DIR'] = 'data/kerko'
 app.config['KERKO_COMPOSER'] = Composer()
-app.config['KERKO_ZOTERO_WAIT'] = '10'
-app.config['KERKOAPP_COLLECTION_FACETS']= 'LKGKEMQ6:110:Positive Deviance Publications'
-app.config['KERKOAPP_EXCLUDE_DEFAULT_FACETS'] = 'facet_tag'
+app.config['KERKO_ZOTERO_WAIT'] = '20'
+
 
 babel = Babel(app)
 bootstrap = Bootstrap(app)
